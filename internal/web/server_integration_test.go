ca0a1154697aa4773229c374a9e7a970eab6275f
Dennis Walter <dennis.walter@gmail.com>
2026-02-27 13:45:44 +0100

    Fix tests.

--- internal/web/server_integration_test.go
+++ internal/web/server_integration_test.go
@@ -83,7 +83,7 @@ func setupIntegrationServer(t *testing.T) (*httptest.Server, *Server) {
 		Version:  "integration-test",
 	}
 
-	server, err := NewServer(opts, st, linter, pluginRegistry, nil, nil)
+	server, err := NewServer(opts, st, linter, pluginRegistry, nil, nil, nil)
 	if err != nil {
 		t.Fatalf("Failed to create server: %v", err)
 	}
